8c3231a3a3f3414eaa508fab1d14dc74657a3a7a
Marian Carnoky <mcarnoky001@cee.ema.ad.pwcinternal.com>
2018-04-30 14:20:01 +0200

    [TASK] added working calendar example

--- frontend/hcconsulting/src/app/app.module.ts
+++ frontend/hcconsulting/src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import  { CalendarComponent } from './calendar/calendar.component';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DemoUtilsModule } from '../utils/module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
-    BrowserModule
+  	CommonModule,
+  	FormsModule,
+  	NgbModalModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    DemoUtilsModule
   ],
+  exports:[CalendarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
